67062669eb92fc62925398d10b17c65cc640d414
kevin.arevalo26 <kevin@orangesoftco.com>
2025-08-24 23:17:50 -0600

    Editing lading page

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,9 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\LandingController;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+Route::get('/', [LandingController::class, 'index'])->name('home');
 
 Route::middleware([
     'auth:sanctum',
